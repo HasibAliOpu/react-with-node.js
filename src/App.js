8fe372a9276623d932c41346a75b7a2986068374
H_A _Opu <wwwhasib4@gmail.com>
2022-04-23 11:09:36 +0600

    showing fake data on ui and implement post method

--- src/App.js
+++ src/App.js
@@ -9,9 +9,41 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    // POST
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+        console.log(data);
+      });
+  };
   return (
     <div className="App">
       <h1>Available User: {users.length}</h1>
+      <form onSubmit={handleCreateUser}>
+        <input type="text" name="name" required placeholder="Name" /> <br />
+        <input type="email" name="email" required placeholder="Email" /> <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} <br /> {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
